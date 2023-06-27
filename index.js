7bc61ee0ce4875ae327ebd0f2ab397d5ca7fa6e4
Harish Kanagal <78932555+Concord1@users.noreply.github.com>
2023-06-27 15:19:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello world!");
+  const PORTO = process.env.PORT;
+  res.end(PORTO);
 });
 
 const PORT = process.env.PORT;
