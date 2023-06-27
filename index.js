596244686f1ef9635bb5ecbfdacd585298f7ac7d
Harish Kanagal <78932555+Concord1@users.noreply.github.com>
2023-06-27 14:13:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log("Server is running on port 3000"));
+server.listen(PORT, () => console.log("Server is running on port" + PORT));
